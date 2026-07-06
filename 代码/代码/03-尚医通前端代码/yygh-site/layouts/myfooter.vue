81c5efe29d491437fe79c84e4f4a567b18ad715d
Ubuntu <ubuntu@ip-172-31-38-8.ap-northeast-1.compute.internal>
2026-07-06 03:38:30 +0000

    Remove footer filing and phone text

--- 代码/代码/03-尚医通前端代码/yygh-site/layouts/myfooter.vue
+++ 代码/代码/03-尚医通前端代码/yygh-site/layouts/myfooter.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="footer-container">
     <div class="wrapper">
-      <div><span class="record">京ICP备13018369号</span><span
-        class="phone">电话挂号010-56253825</span></div>
       <div class="right"><span
         class="v-link clickable"> 联系我们 </span><span
         class="v-link clickable"> 合作伙伴 </span><span
